2d11f12cfd307f97ab11d83e00fbb12f52480705
Om Patel <omunite21@gmail.com>
2025-03-31 23:50:22 +0530

    Step-3: hono error handling

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
+import type { ErrorResponse } from "@/shared/types";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
@@ -6,4 +8,32 @@ app.get("/", (c) => {
 	return c.text("Hello Omi Bhai!");
 });
 
+app.onError((err, c) => {
+	if (err instanceof HTTPException) {
+		const errResponse =
+			err.res ??
+			c.json<ErrorResponse>(
+				{
+					success: false,
+					error: err.message,
+					isFormError:
+						err.cause && typeof err.cause === "object" && "form" in err.cause
+							? err.cause.form === true
+							: false,
+				},
+				err.status,
+			);
+		return errResponse;
+	}
+	return c.json<ErrorResponse>(
+		{
+			success: false,
+			error:
+				Bun.env.NODE_ENV === "production"
+					? "Internal Server Error"
+					: (err.stack ?? err.message),
+		},
+		500,
+	);
+});
 export default app;
